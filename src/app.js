d9a1eda9acee212ebe5aff4805224ea9e749b6ac
Yannick Tirand <ytirand@gmail.com>
2024-05-02 16:35:07 +1200

    Uses dotenv-expand to use env variable in .env files

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@
 import yargs from "yargs"
 import { hideBin } from "yargs/helpers"
 import dotenv from "dotenv";
+import dotenvExpand from "dotenv-expand";
 import project from "./commands/project.js";
 import path from "path";
 import test from "./commands/test.js";
 import git from "./commands/git.js";
 
 const envPath = path.dirname(process.argv[1]) + "/../.env";
-dotenv.config({ path: envPath });
+dotenvExpand.expand(dotenv.config({ path: envPath }));
 
 let ydl = await yargs(hideBin(process.argv))
   .command(project)
